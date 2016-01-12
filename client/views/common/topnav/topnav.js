a700d4db33b274a809e9c4e1b29587cd8d9a355e
Chris Kwan <chriswkwan@gmail.com>
2016-01-12 08:14:31 -0500

    Add to fullname null check

--- client/views/common/topnav/topnav.js
+++ client/views/common/topnav/topnav.js
@@ -14,7 +14,7 @@ Template.topnav.events({
 // Template Helper Map
 Template.topnav.helpers({
     fullname: function(user) {
-        return (user && user.profile) ?
+        return (user && user.profile && user.profile.name) ?
             user.profile.name.first + " " + user.profile.name.last : null;
     },
     friendRequestCount: function() {
