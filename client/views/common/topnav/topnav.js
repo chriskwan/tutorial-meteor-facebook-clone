0e9182cb7633c998ec5f5ddb4bf3de4150f1ac42
Chris Kwan <chriswkwan@gmail.com>
2016-01-12 07:42:12 -0500

    Add events and helpers for topnav template

---
+++ client/views/common/topnav/topnav.js
@@ -0,0 +1,23 @@
+// @description: This is where top nav stuff happens
+
+// Template Event Map
+Template.topnav.events({
+    'click .logout': function() {
+        Meteor.logout(function(err) {
+            if (!err) {
+                Router.go("/");
+            }
+        });
+    }
+});
+
+// Template Helper Map
+Template.topnav.helpers({
+    fullname: function(user) {
+        return (user && user.profile) ?
+            user.profile.name.first + " " + user.profile.name.last : null;
+    },
+    friendRequestCount: function() {
+        return 0;
+    }
+});
